80b6f6d788670905cd58efa63fbfbdfad179908d
Mahmoud-alim <mahmoudhusseinalim@gmail.com>
2021-02-04 14:29:32 +0200

    Retrofit, Gson, Okhttp logging setup

---
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/ui/api/Source.kt
@@ -0,0 +1,6 @@
+package com.androiddevs.mvvmnewsapp.ui.api
+
+data class Source(
+    val id: Any,
+    val name: String
+)
---
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/ui/util/Constants.kt
@@ -0,0 +1,9 @@
+package com.androiddevs.mvvmnewsapp.ui.util
+
+class Constants {
+
+    companion object {
+        const val API_KEY = "6bbbc33d89b84e47bade08b51ce5e39f"
+        const val Base_URL = "https://newsapi.org/"
+    }
+}
