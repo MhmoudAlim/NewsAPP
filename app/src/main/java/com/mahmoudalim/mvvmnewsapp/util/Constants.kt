c16bac240da0efc6f114ce3c9b1d9aff2f0ee6f5
Mahmoud-alim <mahmoudhusseinalim@gmail.com>
2021-02-25 16:25:16 +0200

    removing the realese build APk history

--- app/src/main/java/com/mahmoudalim/mvvmnewsapp/util/Constants.kt
+++ app/src/main/java/com/mahmoudalim/mvvmnewsapp/util/Constants.kt
@@ -5,8 +5,15 @@ class Constants {
     companion object {
 
         const val Base_URL = "https://newsapi.org/"
-        //get your api key -> https://newsapi.org/
-        const val API_KEY = "Add Your Own API Key"
+
+        //        //get your api key -> https://newsapi.org/
+//        const val API_KEY = "Add Your Own API Key"
+//        const val API_KEY = "6bbbc33d89b84e47bade08b51ce5e39f"
+//        const val API_KEY = "eef07fea78ab4997b716877d86ffc562"
+//        const val API_KEY = "ce78bb6e30a54a61925769f15db16d96"
+//        const val API_KEY = "6055b7d8e6544073abfa9779a929639c"
+//        const val API_KEY = "8c6e6afa27304d53828f441421994439"
+        const val API_KEY = "72127f56e0a8452ca149d10dd9a37914"
         const val SEARCH_DELAY_TIME = 1000L
         const val Query_DEFAULT_PAGE_SIZE = 20
         const val SPLASH = 1000L
